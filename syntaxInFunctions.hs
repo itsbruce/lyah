98b5e4a60caa0dc66ee3f58a2bae11f302702306
Bruce Richardson <bruce.richardson@net-a-porter.com>
2015-01-30 11:32:02 +0000

    Better performance on factorial

--- syntaxInFunctions.hs
+++ syntaxInFunctions.hs
@@ -1,5 +1,6 @@
 factorial :: (Integral a) => a -> a
-factorial = let facs = [ fac(x) | x <- [0..] ]
-	        fac 0 = 1
-                fac x = x * facs !! (fromIntegral (x - 1))
-            in  (facs !!) . fromIntegral
+factorial = let go x n
+                    | n < 0 = error "No valid factorial for negatives"
+                    | n > 0 = go (x * n) (n - 1)
+                    | otherwise = x
+	    in  go 1
